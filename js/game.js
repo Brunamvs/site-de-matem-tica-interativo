60f1b3741ad8d43dc4dbee96a78d4af957686e5e
Brunamvs <vieira.silva.bruna@escola.pr.gov.br>
2025-04-22 13:40:12 +0000

    .

---
+++ js/game.js
@@ -0,0 +1,49 @@
+// Variáveis principais do jogo let respostaCorreta;
+let respostaCorreta;
+let pontuacão = 0;
+let modoAtual = 'basico';
+
+
+// Função que inicia o jogo com o modo escolhido
+function iniciarJogo (modo) {
+  modoAtual = modo;
+  document.getElementById ("area-jogo") .style.display = "block";
+  novaPergunta ();
+}
+ 
+
+// Função que gera nova pergunta e mostra na tela
+function novaPergunta ()
+      const numeros = gerarNumeros (modoAtual);
+      const operacao = escolherOpe
+      const perguntaTexto = '${numeros.num1} ${operacao} $ {numeros.num2}';
+
+
+      respostaCorreta = calcularResposta (numeros.num1, numeros.num2,operacao);
+
+
+    document.getElementById ("pergunta") .innerText = perguntaTexto;
+    document.getElementById ("resposta") .value = "";
+
+}
+
+
+// Gera números aleatorios, maiores no modo desafio
+function gerarNumeros (modo) {
+  let num1 = Math.floor(Math.random() * 10) + 1;
+  let num2= Math.floor (Math.random() * 10) + 1;
+
+  if(modo === 'desafio') }
+   num *= 2;
+   num *= 3;
+}
+
+   return { num1, num2 };
+}
+
+// Escolher aleatoriamente uma operacao: +, - ou *
+function escolheroperacao() {
+  const operacoes = ['+', '-', '*'];
+  return operacoes[math.floor(math.random() * operacoes.length)];
+}
+//Calcular o resultado
